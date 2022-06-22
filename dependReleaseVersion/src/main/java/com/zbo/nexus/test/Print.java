624233c5426837577519ce551d681a7001f3791f
zhang_bo <825547140@qq.com>
2022-06-22 19:09:06 +0800

    修改pom文件

---
+++ dependReleaseVersion/src/main/java/com/zbo/nexus/test/Print.java
@@ -0,0 +1,10 @@
+package com.zbo.nexus.test;
+
+public class Print {
+
+    public void view(){
+        Demo d = new Demo();
+        System.out.println(d.getAge());
+        System.out.println(d.getAddr());
+    }
+}
---
+++ nexusReleaseVersion/src/main/java/com/zbo/nexus/test/App.java
@@ -0,0 +1,13 @@
+package com.zbo.nexus.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
