f392302b2528254ed1e275abaaf97e354f17cda5
zhang_bo <825547140@qq.com>
2022-06-23 15:48:48 +0800

    修改代码

--- env_parentProject/web/web-service/src/main/com/zbo/nexus/test/WebLoginServiceImpl.java
+++ env_parentProject/web/web-service/src/main/com/zbo/nexus/test/WebLoginServiceImpl.java
@@ -1,11 +1,16 @@
 package com.zbo.nexus.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebLoginServiceImpl implements WebLoginService {
+
+    @Autowired
+    OrderService orderService;
+
     @Override
     public void login() {
-
+        orderService.findResource();
     }
 }
